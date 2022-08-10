6ed16542210b074e478f92d6cab8e3123e4d01b0
SMAmmadAli <smammadali05@gmail.com>
2022-08-11 04:00:39 +0500

    UI Completed

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,10 @@ class _MyAppState extends State<MyApp> {
             actions: [
               IconButton(
                 onPressed: () {},
-                icon: const Icon(Icons.settings, color: Colors.black45,),
+                icon: const Icon(
+                  Icons.settings,
+                  color: Colors.black45,
+                ),
               )
             ],
           ),
@@ -37,11 +40,57 @@ class _MyAppState extends State<MyApp> {
             child: Padding(
               padding: const EdgeInsets.all(12.0),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(children: [
                     MaleFemale(Icons.male, Colors.blue, 0),
                     MaleFemale(Icons.female, Colors.pink, 1),
-                  ],)
+                  ]),
+                  Container(
+                      margin: const EdgeInsets.only(top: 15.0, left: 10.0),
+                      child: const Text("Your Age in Cm:",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w500,
+                          ))),
+                  Container(
+                      child: TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        hintText: "Your height in Cm", fillColor: Colors.grey),
+                  )),
+                  Container(
+                      margin: const EdgeInsets.only(top: 15.0, left: 10.0),
+                      child: const Text("Your Weight in Kg:",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w500,
+                          ))),
+                  Container(
+                      child: TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        hintText: "Your Weight in Kg", fillColor: Colors.grey),
+                  )),
+                  InkWell(
+                    onTap: (){},
+                    child: AnimatedContainer(
+                      margin: EdgeInsets.only(top: 40.0),
+                      alignment: Alignment.center,
+                      height: 50.0,
+                      width: 100.0,
+                      color: Colors.indigoAccent,
+                      duration: Duration(seconds: 1),
+                      child: Text("Calculate",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w400 
+                                  ),
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
@@ -55,28 +104,28 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  Widget MaleFemale(IconData icon, Color color, int index){
+  Widget MaleFemale(IconData icon, Color color, int index) {
     // var icon;
     return Expanded(
-      child: InkWell(
-        onTap: (){
-          // when the click the button the index will have the value of the Animatedcontainer index
-          changeIndex(index);
-        },
-        child: AnimatedContainer(
-          height: 80.0,
-          margin: const EdgeInsets.symmetric(horizontal: 12.0),
-          duration: const Duration(seconds: 1),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8.0),
-            color: currentindex == index ? color:Colors.grey[200],
-          ),
-          child: Icon(
-            icon,
-            size: 60.0,
-            color: currentindex == index ? Colors.white : color,
-          ),
-          ),
-      ));
+        child: InkWell(
+      onTap: () {
+        // when the click the button the index will have the value of the Animatedcontainer index
+        changeIndex(index);
+      },
+      child: AnimatedContainer(
+        height: 80.0,
+        margin: const EdgeInsets.symmetric(horizontal: 12.0),
+        duration: const Duration(seconds: 1),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.0),
+          color: currentindex == index ? color : Colors.grey[200],
+        ),
+        child: Icon(
+          icon,
+          size: 60.0,
+          color: currentindex == index ? Colors.white : color,
+        ),
+      ),
+    ));
   }
 }
